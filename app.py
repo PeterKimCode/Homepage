8d365dac4d387f3308206f5b3b75bcc59d5c3321
PeterKimCode <kimty5401@gmail.com>
2021-04-25 20:18:26 +0900

    v2

--- app.py
+++ app.py
@@ -8,6 +8,25 @@ def get_db_connection():
     conn.row_factory = sqlite3.Row
     return conn
     
+
+    
+    
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your secret key'
+    
+@app.route('/')
+def index():
+    conn = get_db_connection()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+    conn.close()
+    return render_template('index.html', posts=posts)
+    
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    return render_template('post.html', post=post)
+
+@app.route('/<int:post_id>')
 def get_post(post_id):
     conn = get_db_connection()
     post = conn.execute('SELECT * FROM posts WHERE id = ?',
@@ -28,23 +47,6 @@ def get_post(post_id):
     if post is None:
         abort(404)
     return post
-    
-    
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your secret key'
-    
-@app.route('/')
-def index():
-    conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM posts').fetchall()
-    conn.close()
-    return render_template('index.html', posts=posts)
-    
-@app.route('/<int:post_id>')
-def post(post_id):
-    post = get_post(post_id)
-    return render_template('post.html', post=post)
-
     
 @app.route('/create', methods=('GET', 'POST'))
 def create():
